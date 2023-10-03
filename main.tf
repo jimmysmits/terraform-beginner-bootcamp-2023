2070839e349111c65af7f9a9dc3169d2780419bd
Jimmy Smits <61211859+jimmysmits@users.noreply.github.com>
2023-10-03 21:56:08 +0200

    #13 migrate local state to terraform cloud using terraform cloud block (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "hashicorp-tutorials-learning"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
   required_providers {
     random = {
       source = "hashicorp/random"
